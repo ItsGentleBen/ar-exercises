c87889e02f17bba132ba2afa02b86875bef523e7
Ben Stolb <benstolb@protonmail.com>
2022-12-12 20:47:03 -0800

    Finish 7

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,9 @@ class Store < ActiveRecord::Base
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :gendered_apparel
+
+  def gendered_apparel
+    errors.add(:store, "must cater to at least one gender due to societal norms. Please add men or women's apparel.") unless (mens_apparel? || womens_apparel?)
+  end
 end
