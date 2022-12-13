2fc1355fb675133b0c3066664c033100563c5fcb
Ben Stolb <benstolb@protonmail.com>
2022-12-12 20:26:09 -0800

    Add exercises 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Russel", last_name: "Mueller", hourly_rate: 60)
+@store1.employees.create(first_name: "Jae Chun", last_name: "Yang", hourly_rate: 60)
+@store2.employees.create(first_name: "Satoe", last_name: "Sakonjo", hourly_rate: 60)
+@store2.employees.create(first_name: "Tim", last_name: "Quigg", hourly_rate: 60)
+@store1.employees.create(first_name: "", last_name: "Stolb", hourly_rate: 60)
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter new store name. Be creative!"
+@queried_store = $stdin.gets.chomp
+
+@new_store = Store.create(name: @queried_store)
+
+if !@new_store.valid?
+  puts @new_store.errors.full_messages
+end
